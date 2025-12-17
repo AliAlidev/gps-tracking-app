c260c0bb9e7dc35d404dc8cf7712bc78220942ef
ali ali <ali.ali.deve@gmail.com>
2025-12-17 14:37:34 +0300

    fix

--- lib/core/services/background_service.dart
+++ lib/core/services/background_service.dart
@@ -11,7 +11,7 @@ class BackgroundService {
       taskName,
       frequency: const Duration(minutes: 15),
       constraints: Constraints(
-        networkType: NetworkType.not_required,
+        networkType: NetworkType.notRequired,
         requiresBatteryNotLow: false,
         requiresCharging: false,
         requiresDeviceIdle: false,
